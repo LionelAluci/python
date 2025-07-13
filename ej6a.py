e785232102803100927cf835a46209478507eefa
Aluci <lionelaluci9@gmail.com>
2025-07-13 18:41:57 -0300

    ej6.py

--- ej6a.py
+++ ej6a.py
@@ -1,15 +1,37 @@
-import csv
+def ejercicio6_parte1():
+    archivo = open("cotizacion.csv", "r")
+    lineas = archivo.readlines()
+    archivo.close()
 
-def leer_cotizaciones(nombre_archivo):
-    columnas = {}
-    with open(nombre_archivo, "r") as f:
-        lector = csv.DictReader(f)
-        for fila in lector:
-            for clave, valor in fila.items():
-                if clave not in columnas:
-                    columnas[clave] = []
-                try:
-                    columnas[clave].append(float(valor.replace(",", "").replace("€", "")))
-                except:
-                    columnas[clave].append(valor)
-    return columnas
+    columnas = lineas[0].strip().split(",")
+    datos = {}
+
+    for col in columnas:
+        datos[col] = []
+
+    for linea in lineas[1:]:
+        partes = linea.strip().split(",")
+        for i in range(len(columnas)):
+            datos[columnas[i]].append(partes[i])
+
+    return datos
+
+def ejercicio6_parte2():
+    datos = ejercicio6_parte1()
+    archivo = open("resumen.csv", "w")
+    archivo.write("Columna,Mínimo,Maximo,Media\n")
+
+    for clave in datos:
+        try:
+            numeros = [float(x) for x in datos[clave]]
+            minimo = min(numeros)
+            maximo = max(numeros)
+            media = sum(numeros) / len(numeros)
+            archivo.write(f"{clave},{minimo},{maximo},{media}\n")
+        except:
+            pass  # salta columnas con texto
+
+    archivo.close()
+    print("Resumen guardado.")
+
+ejercicio6_parte2()
