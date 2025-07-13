ea8b513e79611124c0f157539a8cbfee4875b5b7
Aluci <lionelaluci9@gmail.com>
2025-07-13 18:51:41 -0300

    ej3.py

--- ej3.py
+++ ej3.py
@@ -1,15 +1,12 @@
-def mostrar_linea():
-    n = int(input("Introduce el número de la tabla (1 a 10): "))
-    m = int(input("Introduce la línea que quieres ver (1 a 10): "))
-    nombre_archivo = f"tabla-{n}.txt"
+def ejercicio3():
+    n = int(input("Número (1-10): "))
+    m = int(input("Línea (1-10): "))
     try:
-        with open(nombre_archivo, "r") as archivo:
-            lineas = archivo.readlines()
-            if 1 <= m <= 10:
-                print(f"Línea {m}: {lineas[m-1]}")
-            else:
-                print("La línea está fuera de rango.")
-    except FileNotFoundError:
-        print(f"No existe el archivo {nombre_archivo}")
+        archivo = open("tabla-" + str(n) + ".txt", "r")
+        lineas = archivo.readlines()
+        print("Línea", m, ":", lineas[m - 1])
+        archivo.close()
+    except:
+        print("El archivo no existe o la línea es incorrecta.")
 
-mostrar_linea()
+ejercicio3()
