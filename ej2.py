15eed611422273b714904727dc5355ef0586017f
Aluci <lionelaluci9@gmail.com>
2025-07-16 08:08:24 -0300

    ej2.py

--- ej2.py
+++ ej2.py
@@ -1,10 +1,12 @@
 def ej2():
-    n = int(input("Número (1-10): "))
+    n = int(input("numero (1-10): "))
     try:
         archivo = open("tabla-" + str(n) + ".txt", "r")
         print(archivo.read())
         archivo.close()
-    except:
-        print("El archivo no existe.")
+    except ValueError:
+        print("ingresa un numero valido")
+    except FileNotFoundError:
+        print("el archivo no existe")
 
 ej2()
